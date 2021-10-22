f5e56912b37ac0aca77cb0fc610d8dec96305948
xAzKaR <leandro.glc@gmail.com>
2021-10-21 21:17:05 -0300

    syst

--- login-php/sistema.php
+++ login-php/sistema.php
@@ -28,7 +28,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">SISTEMA DO GN</a>
+            <a class="navbar-brand" href="#">Sistema Healthy Lieah</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
